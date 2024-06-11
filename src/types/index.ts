e2d583cafcaf25c2d11fc80e2035aaa8888d69fe
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2024-06-11 10:46:11 +0530

    add button types

--- src/types/index.ts
+++ src/types/index.ts
@@ -14,3 +14,24 @@ export type Product = {
    rating: number
    total_rating_feedbacks: number
 }
+
+export type VoidElement =
+   | 'area'
+   | 'base'
+   | 'br'
+   | 'col'
+   | 'hr'
+   | 'img'
+   | 'input'
+   | 'link'
+   | 'meta'
+   | 'param'
+   | 'command'
+   | 'keygen'
+   | 'source'
+
+export type OmitChildrenFromVoid<C extends React.ElementType> = C extends VoidElement
+   ? Omit<React.ComponentPropsWithRef<C>, 'children'>
+   : React.ComponentPropsWithRef<C>
+
+export type DistributiveOmit<T, K extends keyof any> = T extends any ? Omit<T, K> : never
