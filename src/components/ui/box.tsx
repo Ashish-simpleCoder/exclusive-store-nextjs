c6e97502528cde64d28d4c535d9b02e09794766d
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2024-06-11 11:13:19 +0530

    feat: add overloading for Box component with new As prop

--- src/components/ui/box.tsx
+++ src/components/ui/box.tsx
@@ -1,16 +1,42 @@
-import cn from '@/lib/cn'
-import { ComponentPropsWithoutRef } from 'react'
+import type { ComponentPropsWithRef, ElementType } from 'react'
+import type { DistributiveOmit, OmitChildrenFromVoid } from '@/types'
 
-export default function Box<Tag extends keyof HTMLElementTagNameMap = 'span'>(
-   props: ComponentPropsWithoutRef<Tag> & { as?: Tag }
-) {
-   const { as = 'span', className, ...rest } = props
-   const Tag = as
+function Box<As extends ElementType>(
+   props: DistributiveOmit<OmitChildrenFromVoid<As>, keyof {}> & { As: As }
+): JSX.Element
+function Box<As = 'span'>(props: ComponentPropsWithRef<'span'>): JSX.Element
+function Box({ As: Component = 'span', ...props }) {
+   const { className, ...rest } = props
 
    return (
       <>
-         {/* @ts-ignore */}
-         <Tag className={cn('', className)} {...(rest as any)} />
+         <Component
+            // @ts-ignore
+            className={className}
+            {...rest}
+         />
       </>
    )
 }
+export default Box
+// export default function Container(props: ComponentProps<keyof HTMLElementTagNameMap> & {As?: keyof JSX.IntrinsicElements}) {
+
+
+
+// ---------------------        old implementiona -------------------------------
+// import cn from '@/lib/cn'
+// import { ComponentPropsWithoutRef } from 'react'
+
+// export default function Box<Tag extends keyof HTMLElementTagNameMap = 'span'>(
+//    props: ComponentPropsWithoutRef<Tag> & { as?: Tag }
+// ) {
+//    const { as = 'span', className, ...rest } = props
+//    const Tag = as
+
+//    return (
+//       <>
+//          {/* @ts-ignore */}
+//          <Tag className={cn('', className)} {...(rest as any)} />
+//       </>
+//    )
+// }
